33f011a3d1ba2eb655135d917a024b085de2b9ea
Baran Meric <37614053+baranmeric@users.noreply.github.com>
2026-03-23 01:00:33 +0100

    remove moved files from api

--- apps/api/src/router/chat.router.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import {chatSchema, validateRequest} from "../middleware/validation";
-import {ChatController} from "../controller/chat.controller";
-
-const router = Router();
-router.post('/:id', validateRequest(chatSchema), ChatController.chat);
-
-export default router;
